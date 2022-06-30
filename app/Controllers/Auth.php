63c27a8c9f81a93825eadc97c36d112930794cbe
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2022-06-30 16:16:14 -0500

    IntentoTerminado

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -9,50 +9,58 @@ class Auth extends BaseController
 
     public function login()
     {
-        $users = new UserModel();
-        $user = $users->findById(1);
-        $token = $user->generateAccessToken('Work Laptop');
-        return $token->raw_token;
-        // $user->revokeAllAccessTokens();
+        $validation = $this->validate([
+            'email' => 'required|valid_email',
+            'password' => 'required|min_length[8]|max_length[12]',
+        ]);
+        if (!$validation) {
+            return $this->validator->getErrors();
+        } else {
+            $email = $this->request->getVar('email');
+            $password = $this->request->getVar('password');
 
-        // $user->revokeAccessToken('d71bb2ddb267c60049abd5b21b34a730da63f52fff297862585a0b147c603b56');
-        // return $usrs;
+            $users = new UserModel();
+            $user = $users->where('email', $email)->first();
 
+            $exitoso = password_verify($password, $user->password);
+            if (!$exitoso) {
+                return "Credenciales Incorrectas";
+            }
+            $token = $user->generateAccessToken('Web');
+            return $token->raw_token;
+            return 'Exitoso';
+        }
     }
     public function logout()
     {
-        return json_encode("ahuvo");
-        // $users = new UserModel();
-        // $user = $users->findById(1);
-        // $user->revokeAllAccessTokens();
-        // $user->revokeAccessToken('d71bb2ddb267c60049abd5b21b34a730da63f52fff297862585a0b147c603b56');
-        // return $usrs;
-
+        $user= auth()->user();
+        $user->revokeAllAccessTokens();
+        return "LoggedOut";
     }
     public function register()
     {
-           $validation = $this->validate([
-                'name'=> 'required|min_length[6]|is_unique[users.email]',
-                'email' => 'required|valid_email|is_unique[users.email]',
-                'password' => 'required|min_length[8]|max_length[12]',
-                'cpassword' => 'required|min_length[8]|max_length[12]|matches[password]',
-            ]);
-            if (!$validation) {
-                return $this->validator->getErrors();
-            }else {
-                $name = $this->request->getVar('name');
-                $email = $this->request->getVar('email');
-                $password = $this->request->getVar('password');
-                $cpassword = $this->request->getVar('cpassword');
-                $values = [
-                    'username' => $name,
-                    'email' => $email,
-                    'password' => password_hash($password,PASSWORD_BCRYPT),
-                    'cpassword' => $cpassword,
-                ];
-                $userModel = new UserModel();
-                $query = $userModel->insert($values);
-                return 'Exitoso';
-            }
+        $validation = $this->validate([
+            'name' => 'required|min_length[6]|is_unique[users.email]',
+            'email' => 'required|valid_email|is_unique[users.email]',
+            'password' => 'required|min_length[8]|max_length[12]',
+            'cpassword' => 'required|min_length[8]|max_length[12]|matches[password]',
+        ]);
+        if (!$validation) {
+            return $this->validator->getErrors();
+        } else {
+            $name = $this->request->getVar('name');
+            $email = $this->request->getVar('email');
+            $password = $this->request->getVar('password');
+            $cpassword = $this->request->getVar('cpassword');
+            $values = [
+                'username' => $name,
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_BCRYPT),
+                'cpassword' => $cpassword,
+            ];
+            $userModel = new UserModel();
+            $query = $userModel->insert($values);
+            return 'Exitoso';
+        }
     }
 }
